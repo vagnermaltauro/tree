d29ab02bbacb91f3e6713a75789a08882c02db6c
vagnermaltauro <vagnermaltauro@gmail.com>
2023-10-05 17:01:09 -0300

    feat: add Lucide and component Icons

---
+++ components/icons.tsx
@@ -0,0 +1,10 @@
+import { Banana, File, Folder, Loader2, LucideProps } from 'lucide-react';
+
+export type Icon = LucideProps;
+
+export const Icons = {
+  folder: Folder,
+  file: File,
+  spinner: Loader2,
+  brand: Banana,
+};
